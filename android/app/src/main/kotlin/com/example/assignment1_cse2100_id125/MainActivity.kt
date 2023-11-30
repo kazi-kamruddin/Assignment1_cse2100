b1b3e47324f99d8e6ef3097a838dfe74177daf6f
shourov <kazikamruddinahmed@gmail.com>
2023-11-30 11:06:41 +0600

    initial commit

---
+++ android/app/src/main/kotlin/com/example/assignment1_cse2100_id125/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.assignment1_cse2100_id125
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ProductDetailsPage(),
+    );
+  }
+}
+
+class ProductDetailsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey[600],
+      appBar: AppBar(
+        title: Text("Greek assassin"),
+        centerTitle: true,
+        backgroundColor: Colors.blueGrey[800],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(30, 20, 30, 10),
+        child: Column(
+          // crossAxisAlignment: CrossAxisAlignment.center,
+          // mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(80),
+                  child: Image.asset(
+                    'chobi/kassandra.jpg',
+                    height: 200,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Container(
+              decoration: BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(
+                    color: Colors.black,
+                    width: 2,
+                  ),
+                ),
+              ),
+              child: Text(
+                'kassandra',
+                style: TextStyle(
+                  fontSize: 23,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Container(
+              child: Text(
+                'According to Tech Jury, despite a number of cool apps and tips for successful time management, only 17% of people track their time. 50% of people have never thought about time waste, even though they are always late and running out of time. Time management is a skill. It helps people handle their daily duties without burnout and severe exhaustion. The N.I.L.C. includes time management on the list of top ten demanded soft skills that employees require in 2022. Why is it so important to manage one’s time correctly? Stephen Covey once said, “The key is not spending time, but in investing it”. It means that proper timing guarantees a person’s success in many life areas.',
+                style: TextStyle(
+                  fontSize: 12,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text("click this"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
